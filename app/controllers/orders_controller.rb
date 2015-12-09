0e1f796aa62d4b4d2d65a8809bf3e2173c4bf6b3
ComandaKOK <rmkaporin@gmail.com>
2015-12-09 23:41:04 +0200

    Logic fix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -59,7 +59,7 @@ class OrdersController < ApplicationController
     if signed_in?
       redirect_to orders_path
     else
-      redirect_to complete_path
+      redirect_to @order
     end
   end
 
